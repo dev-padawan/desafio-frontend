5eeeaa8117b1564464f99a128bd914ff7bc766b6
douglasmatosdev <douglasmatosdev@gmail.com>
2019-11-24 22:34:03 -0300

    Capiturando dados da API e inserindo no HMTL

---
+++ public/assets/js/exibeDados.js
@@ -0,0 +1,59 @@
+function exibeDados(clima) {
+  
+  criaElementos(clima);
+};
+
+function criaElementos(clima) {
+
+  let widgetWeather   = document.querySelector('.widgetWeather');
+  
+  let localizacao     = document.createElement('h2');
+  let temperatura     = document.createElement('h1');
+  let moreInfoWeather = document.createElement('div');
+  let lowAndHigh      = document.createElement('span');
+  let sensationTermic = document.createElement('span');
+  let wildSpeed       = document.createElement('span');
+  let humidity        = document.createElement('span');
+  let forecastForWeek = document.createElement('ul');
+  let primeio         = document.createElement('ul');
+  let segundo         = document.createElement('ul');
+  let terceiro        = document.createElement('ul');
+  let quarto          = document.createElement('ul');
+  let quinto          = document.createElement('ul');
+  
+  localizacao.className     = "location";
+  temperatura.className     = "infoWeather";
+  moreInfoWeather.className = "moreInfoweather";
+  lowAndHigh.className      = "moreInfoWeather_item lowAndHigh";
+  sensationTermic.className = "moreInfoWeather_item sensationTermic";
+  wildSpeed.className       = "moreInfoWeather_item wildSpeed";
+  humidity.className        = "moreInfoWeather_item humidity";
+  primeio.className         = "forecastForWeek_item primeiro";
+  segundo.className         = "forecastForWeek_item segundo";
+  terceiro.className        = "forecastForWeek_item terceiro";
+  quarto.className          = "forecastForWeek_item quarto";
+  quinto.className          = "forecastForWeek_item quinto";
+
+  localizacao.innerHTML     = `${clima.cidade}, ${clima.estado} - ${clima.pais}`;
+  temperatura.innerHTML     = `${clima.temperatura}C° ${clima.condicaoClima}`;
+  lowAndHigh.innerHTML      = `${clima.temperaturaMinima}C° ${clima.temperaturaMaxima}C°`;
+  sensationTermic.innerHTML = `Sensação ${clima.sensacaoTermica}C°`;
+  wildSpeed.innerHTML       = `Vento ${clima.vento}Km/h`;
+  humidity.innerHTML        = `Humidade ${clima.humidade}%`;
+  
+  widgetWeather.appendChild(localizacao);
+  widgetWeather.appendChild(temperatura);
+  widgetWeather.appendChild(moreInfoWeather);
+  widgetWeather.appendChild(forecastForWeek);
+  moreInfoWeather.appendChild(lowAndHigh);
+  moreInfoWeather.appendChild(sensationTermic);
+  moreInfoWeather.appendChild(wildSpeed);
+  moreInfoWeather.appendChild(humidity);
+  forecastForWeek.appendChild(primeio);
+  forecastForWeek.appendChild(segundo);
+  forecastForWeek.appendChild(terceiro);
+  forecastForWeek.appendChild(quarto);
+  forecastForWeek.appendChild(quinto);
+
+  console.log(widgetWeather);
+}
---
+++ public/assets/js/obtemDados.js
@@ -0,0 +1,59 @@
+function obtemDados(dados) {
+  // console.log(dados);
+  const { 
+    current_observation: climaAtual,
+    forecasts: previsao,
+    location: localizacao
+  } = dados;
+    
+  let { atmosphere, condition, wind, pubDate} = climaAtual;
+  let { humidity } = atmosphere;
+  let { speed, chill } = wind;
+  let { text, temperature } = condition;
+  let { city, region, country } = localizacao;
+  
+  let today = new Date(pubDate * 1000);
+  today = today.toLocaleDateString('pt-Br');
+
+  const clima = {
+    cidade: city,
+    estado: region,
+    pais: country,
+    temperatura: temperature,
+    temperaturaMinima: '',
+    temperaturaMaxima: '',
+    sensacaoTermica: chill,
+    condicaoClima: translate(text),
+    humidade: humidity,
+    vento: speed,
+    diaAtual: today,
+  }
+
+  previsao.forEach(element=> {
+    var dia = new Date(element.date * 1000);
+    dia = dia.toLocaleDateString('pt-BR');
+    
+    if( dia === today) {
+      var {  low, high } = element;
+      clima.temperaturaMinima = low;
+      clima.temperaturaMaxima = high;
+    }
+  });
+  
+  exibeDados(clima); 
+  // console.log(`
+  // ${clima.cidade}, ${clima.estado} - ${clima.pais}
+  // Vento: ${clima.vento}Km
+  // Condicao: ${clima.condicaoClima}
+  // Temp: ${clima.temperatura}C°
+  // Temperatura mínima: ${temperaturaMinima}
+  // Temperatura máxima: ${temperaturaMaxima}
+  // Sensação Térmica: ${clima.sensacaoTermica}Cº
+  // Data: ${clima.diaAtual}
+  // Humidade: ${clima.humidade}%
+  
+  // `);
+  
+  // document.querySelector('.location').innerHTML = `${city}, ${region} - ${country}` ;
+ return clima;
+}
